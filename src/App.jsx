c15dff12350558c7831bf1d9160316eb28a37c64
Sadman Al Sakib <saadman@Sadmans-MacBook-Air.local>
2025-09-30 22:36:30 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <input type='text' placeholder='What To Do...'></input>
+      <button>ADD</button>
+    </div>
+  )
+}
+
+export default App
